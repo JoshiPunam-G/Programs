aceeb7fe76e7d2150ed6b908b3e1aa19820f8b61
supriyaAdak <supriyakadak24@gmail.com>
2019-12-13 16:54:27 +0530

    Initial commit

---
+++ Documents/Automation/src/main/java/com/bridgelabz/selenium/GmailAutomation.java
@@ -0,0 +1,96 @@
+package com.bridgelabz.selenium;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class GmailAutomation {
+	static WebDriver driver;
+	
+	public static void uploadFileWithRobot ()
+	{
+		StringSelection str=new StringSelection("/home/admin106/Documents/ReadExcel.xlsx");
+		Clipboard clipboard=Toolkit.getDefaultToolkit().getSystemClipboard();
+		clipboard.setContents(str, null);
+		Robot robot=null;
+		try {
+		 robot=new Robot();
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+		
+		robot.delay(250);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.delay(150);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+		//return filepath;
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		 
+		System.setProperty("webdriver.chrome.driver", "/home/admin106/Downloads/chromedriver_linux64/chromedriver");
+		 driver = new ChromeDriver();
+		
+		/**Open gmail login page*/ 
+		driver.navigate().to("http://mail.google.com");
+		
+		/** Enter Username */
+		driver.findElement(By.name("identifier")).sendKeys("toolselenium07@gmail.com");
+		driver.findElement(By.id("identifierNext")).click();
+		Thread.sleep(2000);
+		
+        /** Enter password */
+		WebElement passwordButton = driver.findElement(By.xpath("//input[@class='whsOnd zHQkBf']"));
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.elementToBeClickable(passwordButton));
+		passwordButton.sendKeys("tool@123");
+		
+		/**Submit */
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@id='passwordNext']")).click();
+		Thread.sleep(2000);
+		
+		/**Compose Mail */
+		driver.findElement(By.xpath(" //div[@class='T-I J-J5-Ji T-I-KE L3']")).click();
+		Thread.sleep(2000);
+		
+		/**Sent Mail  */
+		driver.findElement(By.xpath("//textarea[@name='to']")).sendKeys("punamsj20@gmail.com");
+		driver.findElement(By.xpath("//input[@name='subjectbox']")).sendKeys("Automated Record File");
+		
+		/**Attach File */
+		driver.findElement(By.xpath("//div[@class='Am Al editable LW-avf tS-tW']")).sendKeys("Hello Punam ..! This is Automated Email ");
+		driver.findElement(By.xpath("//div[@class='a1 aaA aMZ']")).click();
+		GmailAutomation.uploadFileWithRobot();
+		
+		/**Send Mail */
+		driver.findElement(By.xpath("//div[@class='T-I J-J5-Ji aoO v7 T-I-atl L3']")).click();
+		Thread.sleep(2000);
+		
+		/**Click On Google Account */
+		driver.findElement(By.xpath("//span[@class='gb_Ia gbii']")).click();
+		
+		/**Sign Out Account */
+		driver.findElement(By.xpath("//a[@id='gb_71']")).click();
+		
+		driver.close();
+	}
+
+
+}
---
+++ Documents/Automation/src/main/java/com/bridgelabz/selenium/LinkedInLogin.java
@@ -0,0 +1,52 @@
+package com.bridgelabz.selenium;
+import java.util.concurrent.TimeUnit;
+
+import java.io.File;
+import java.io.IOException;
+import junit.framework.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.testng.annotations.Test;
+
+
+public class LinkedInLogin {
+	
+//	 @Test(dataProvider="linkedInData")
+//     public void loginLinkedIn(String username,String password) throws InterruptedException
+//     {
+//		 System.setProperty("webdriver.chrome.driver", "/home/admin106/Downloads/chromedriver_linux64/chromedriver");
+//    	 WebDriver driver=new ChromeDriver();
+// 		 driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+// 		 driver.manage().window().maximize();
+// 		 driver.navigate().to("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin");
+// 		 Thread.sleep(2000);
+// 		 driver.findElement(By.id("username")).sendKeys(username);
+// 		 driver.findElement(By.xpath("//input[@name='session_password']")).sendKeys(password);
+// 		 Thread.sleep(2000);
+// 		 driver.findElement(By.xpath("//button[@class='btn__primary--large from__button--floating']")).click();
+// 		 Thread.sleep(2000);
+// 		
+//     }
+//	 
+//	 @DataProvider(name="linkedInData")
+//	 public Object[][] passData()
+//	 {
+//		Object[][] data=new Object[3][2];
+//		
+//		data[0][0]="punamsj20@gmail.com";
+//		data[0][1]="123";
+//		
+//		data[1][0]="seleniumtoo@gmail.com";
+//		data[1][1]="tool@123";
+//		
+//		data[2][0]="joshipunam207@gmail.com";
+//		data[2][1]="7218374140";
+//		
+//		return data;
+//	 }
+		
+
+}
